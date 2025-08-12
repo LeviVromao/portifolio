20975483eec542f4ccbb580f40408fdeee101d80
NoOneLevi <zxvitor1@hotmail.com>
2025-08-12 03:36:59 -0300

    happy

--- pages/api/likes.ts
+++ pages/api/likes.ts
@@ -3,15 +3,15 @@ import { makeLikesWork, getLikes } from "../../services/handleLikes";
 
 export default async function handler(req, res) {
     
-    if(req.method === 'POST') {
-        await createConnection()
-        const { like } = req.body;
-        const id = await makeLikesWork(`${like}`)
-        res.json({id})
+    //if(req.method === 'POST') {
+    //    await createConnection()
+    //    const { like } = req.body;
+    //    const id = await makeLikesWork(`${like}`)
+    //    res.json({id})
 
-    } else if(req.method === 'GET') { 
-        await createConnection()
-        const data = await getLikes()
-        res.json(data);
-    }
+//    } else if(req.method === 'GET') { 
+//        await createConnection()
+//        const data = await getLikes()
+//        res.json(data);
+//    }
 }
